760123128246401daa5418d6c394d4d71ac02994
unknown <fazlerabbi2248@gmail.com>
2022-09-02 17:47:18 +0600

    create a own custom package and publish

---
+++ rabbiCalculator/__init__.py
@@ -0,0 +1,8 @@
+def add_numbers(a,b):
+    return a+b
+def mul_numbers(a,b):
+    return a*b
+def sub_numbers(a,b):
+    return a-b
+def div_numbers(a,b):
+    return a/b
---
+++ setup.py
@@ -0,0 +1,14 @@
+import setuptools
+setuptools.setup(
+ name='rabbiCalculator',
+ version='0.1',
+ author="fazle rabbi",
+ author_email="fazlerabbi2248@gmail.com",
+ description="use for calculate two value",
+ packages=setuptools.find_packages(),
+ classifiers=[
+ "Programming Language :: Python :: 3",
+ "License :: OSI Approved :: MIT License",
+ "Operating System :: OS Independent",
+ ],
+)
